36e3cb7dca8bb59ee79759805cfed8a1e633b272
shubhamd2201 <dubeyshubham2201@gmail.com>
2023-09-18 03:42:51 +0530

    added prototype

--- doorvision/js/custom.js
+++ doorvision/js/custom.js
@@ -26,10 +26,10 @@ size_button.click(function(){
     size_button.removeClass('selected');
     $(this).addClass('selected');
      door_size = $(this).attr('data-tab');
-     $('#width_for_quatation').text(null); 
-    $('#width_for_quatation').text("8ft, 0 inch");
-    $('#height_for_quatation').text(null); 
-    $('#height_for_quatation').text("7ft, 0 inch");
+    //  $('#width_for_quatation_ft').text(null); 
+    // $('#height_for_quatation').text(null);
+    $('#width_for_quatation_ft').text($(this).attr('width_from_btn')+ "ft 0inch");
+    $('#height_for_quatation').text($(this).attr('height_from_btn') + "ft 0inch"); 
 
 
 });
@@ -49,25 +49,25 @@ collection_and_family_btn.click(function(){
     $('.model_num_row').hide();
     $('.long_short_type').hide();
 
+    // $('#collection_for_quatation').text(null);
+    $('#collection_for_quatation').text($(this).text() + " collection");
+
+
     
 });
 
-collection_and_family_type_btn.click(function(){
-    collection_and_family_type_btn.removeClass('selected');
-    $(this).addClass('selected');
-    $('.model_num_row').hide();
-    $('.long_short_type').hide();
-    collection_and_family_type = $(this).attr('data-tab'); 
 
-});
 $('.raised_type').click(function(){
     $('.raised_type').removeClass('selected');
     $('.model_num_row').hide();
     $('.long_short_type').hide();
-    $('.long_short_type').hide();
     $(this).addClass('selected');
-    $(this).attr('data-tab');
-    $('#'+$(this).attr('data-tab')).show();
+    collection_and_family_type = $(this).attr('data-tab');
+    $('#'+collection_and_family_type).show();
+
+    // $('#family_for_quatation').text(null);
+    $('#family_for_quatation').text(" ," +$(this).text() + " family");
+
 });
 
 
@@ -82,9 +82,11 @@ panel_type_btn.click(function(){
     $('.window_type ul#'+panel_type_value).show();
 
     $('.model_num_row#'+$(this).attr('model-tab')).show();
-    
 
     window_panel_type = $(this).attr('window');
+
+    // $('#panel_type_for_quatation').text(null);
+    $('#panel_type_for_quatation').text(" ," +$(this).text());
 
 
 });
@@ -95,6 +97,9 @@ model_number.click(function(){
     model_number_value = $(this).attr('data-tab'); 
     $('#model_number_img img').attr('src',"./images/"+model_number_value+'.png');
     $('#model_number_img').show();
+
+    $('#model_number_for_quatation').text(null);
+    $('#model_number_for_quatation').text(" ," + $(this).find('.model_num').text());
 });
 
 $('#select_color ul li').click(function(){
@@ -103,6 +108,9 @@ $('#select_color ul li').click(function(){
     panel_color = $(this).attr('dapplyColorOverlaata-color');
     $('#grid_submit').show();
 
+
+    // $('#color_for_quatation').text(null);
+    $('#color_for_quatation').text(", "+$(this).text()+" color");
 
 });
 
@@ -138,6 +146,9 @@ $("#window_glass ul li").click(function(){
     $(this).addClass('selected');
     glass_type = $(this).attr('data-glass');
 
+    // $('#window_glass_type_for_quatation').text(null);
+    $('#window_glass_type_for_quatation').text(", "+$('.window_glass_tabbing ul li.selected').text() +" "+ $(this).text());
+
 
     $('.image_grid_parent.append_grid ul li.i_am_selected').find('span').css('background' , 'url(./images/glass/'+glass_type+'.jpg)');
 });
@@ -146,6 +157,10 @@ $('#window_type ul li').click(function(){
     $("#window_type ul li").removeClass('selected');
     $(this).addClass('selected');
     window_type = $(this).attr('data-window');
+
+    // $('#window_type_for_quatation').text(null);
+    $('#window_type_for_quatation').text($(this).text());
+
 
     if($(".image_grid_parent.append_grid ul li img").length > 0){
         applyColorOverlay_multiple(this_color);
@@ -208,13 +223,25 @@ function grid_append(grid_length){
         document.querySelectorAll('.append_grid ul').forEach(e=>{
             e.innerHTML = "";
             
-            // console.log(grid_length);
         });
         document.querySelectorAll('.append_grid ul').forEach(e=>{
             for(let i = 0; i < grid_length; i++){
             e.insertAdjacentHTML('beforeend' , '<li></li>');
             }
         });
+
+        // this is dummy function 
+        let height_arr = [21, 21, 21,21];
+
+        document.querySelector('.for_all_grid ul').innerHTML="";
+        document.querySelector('.for_grid_height ul').innerHTML = "";
+        for(let i = 0; i < 4; i++){
+            document.querySelector('.for_all_grid ul').insertAdjacentHTML("beforeend", `<li><span>All</span></li>`);
+
+        document.querySelector('.for_grid_height ul').insertAdjacentHTML('beforeend', `<li><span>${height_arr[i]}</span></li>`);
+    }
+        // dummy function end 
+
 
         document.querySelector('.image_grid_parent.append_grid ul').className = '';
         document.querySelector('.image_grid_parent.append_grid ul').classList.add(panel_type_value+'-'+door_size);
@@ -229,8 +256,54 @@ function grid_append(grid_length){
                 this.classList.toggle('selected');  
                 quantity_of_window = $(this).siblings('li.selected').length + 1;
                 $('.window_quantity h4').text(quantity_of_window);
+
+                // $('#window_quantity_for_quatation').text(null);
+                $('#window_quantity_for_quatation').text('window quantity is - '+ quantity_of_window);
+
             });
         });
+
+        // dummy function click 
+        let for_select_box = []
+        document.querySelectorAll('.select_boxes ul li').forEach((e, i)=>{
+            for_select_box.push(i);
+        });
+        
+        let partSize = for_select_box.length / 4;
+        let parts = [];
+        for (let i = 0; i < 4; i++) {
+            let startIndex = i * partSize;
+            let endIndex = startIndex + partSize;
+            let part = for_select_box.slice(startIndex, endIndex);
+            parts.push(part);
+          }
+
+        document.querySelectorAll('.for_all_grid ul li span').forEach((e, index_this)=>{
+            e.addEventListener('click', function(){
+                
+                this.classList.toggle('selected');
+                console.log(parts[index_this][0]);
+                console.log(parts[index_this].length);
+                console.log(this.className)
+                
+                if(this.className == "selected"){
+                    for(i = parts[index_this][0]; i < (parts[index_this][0] + parts[index_this].length); i++){
+                    document.querySelectorAll('.select_boxes ul li')[i].classList.add('selected');
+                    }
+                }
+                else{
+                    for(i = parts[index_this][0]; i < (parts[index_this][0] + parts[index_this].length); i++){
+                        document.querySelectorAll('.select_boxes ul li')[i].classList.remove('selected');
+                        } 
+                }
+
+                let length_of_window  = $('.select_boxes ul li.selected').length;
+                $('.window_quantity h4').text(length_of_window);
+                $('#window_quantity_for_quatation').text("window quantity is - "+length_of_window);  
+            });
+        });
+
+        // dummy function click end
 
     }
 
@@ -256,14 +329,15 @@ function grid_append(grid_length){
         
         
         for(let i = 0; i < index_num_arr.length; i++){
-            document.querySelectorAll('.image_grid_parent.append_grid ul li')[index_num_arr[i]].insertAdjacentHTML('beforeend' , `<img class='window_frame'/><canvas class="myCanvas"></canvas><span></span`);
+            document.querySelectorAll('.image_grid_parent.append_grid ul li')[index_num_arr[i]].insertAdjacentHTML('beforeend' , '<img class=window_frame"/><canvas class="myCanvas"></canvas><span></span>');
             document.querySelectorAll('.image_grid_parent.append_grid ul li')[index_num_arr[i]].classList.add('i_am_selected');
 
-            document.querySelectorAll('.image_grid_parent.append_grid ul li')[index_num_arr[i]].querySelector('img').src = `images/${window_type}.png`;
+            document.querySelectorAll('.image_grid_parent.append_grid ul li')[index_num_arr[i]].querySelector('img').src = `images/window/${panel_type_value}/${window_type}.png`;
 
-            $('.image_grid_parent.append_grid ul li.i_am_selected').find('span').css('background' , 'url(./images/glass/'+glass_type+'.jpg)');
+            $('.image_grid_parent.append_grid ul li.i_am_selected').find('span').css('background' , 'url(images/glass/'+glass_type+'.jpg)');
+
+
         }
-        console.log(`${window_type}`)
 
         applyColorOverlay_multiple(this_color);
 
@@ -277,4 +351,43 @@ function grid_append(grid_length){
         $(this).parent().find('li.selected').removeClass('selected');
         $(this).addClass('selected');
     });
+
+    $(".for_spring_quatation ul li").click(function(){
+
+        $("#Spring_for_quatation").text($(this).text());
+        $('#cyclage_for_quatation').text($('.Cyclage option:first').text());
+    });
+
+    $('.seals_quataion ul li').click(function(){
+        $('#seal_type_for_quatation').text($(this).text());
+
+    });
+
+    $('.lock_bar ul li').click(function(){
+        $('#lock_for_quatation').text($(this).text());
+    });
+
+    $('.jamb_mount ul li').click(function(){
+        $('#jamb_mount_for_quatation').text($(this).text());
+    });
+
+    $('.jamb_material ul li').click(function(){
+        $('#jamb_material_for_quatation').text($(this).text());
+    });
+
+
+
+    $(".track_row .track_sibilings ul li").click(function(){
+        $(".track_row .track_sibilings ul li").not($(this).parent().parent().siblings().find('ul li')).removeClass('selected');
+        
+        $(this).addClass('selected');
+       
+        $('#track_for_quatation').text($('.track_row').find('.selected').text());
+        console.log($('.track_row').find('.selected').text());
+
+    });
+
+    $('.strut_type ul li').click(function(){
+        $('#strut_for_quatation').text($(this).text());
+    })
 
--- doorvision/js/download-image.js
+++ doorvision/js/download-image.js
@@ -52,31 +52,50 @@ function applyColorOverlay(my_color) {
 
 
   
-  function applyColorOverlay_multiple(my_color) {
+//   function applyColorOverlay_multiple(my_color) {
 
-  document.querySelectorAll('.myCanvas').forEach(e=>{
-  var ctx = canvas.getContext('2d');
-  var image = document.querySelector('.image_grid_parent.append_grid ul li.i_am_selected img.window_frame');
-  var maskCanvas = document.createElement('canvas');
-  var maskCtx = maskCanvas.getContext('2d');
+//     document.querySelectorAll('.myCanvas').forEach(e=>{
+//     var ctx = e.getContext('2d');
+//     var image = document.querySelector('.image_grid_parent.append_grid ul li.i_am_selected img');
 
-  canvas.width = image.width;
-  canvas.height = image.height;
-  maskCanvas.width = image.width;
-  maskCanvas.height = image.height;
+//     e.width = image.width;
+//     e.height = image.height;
 
-  maskCtx.drawImage(image, 0, 0, maskCanvas.width, maskCanvas.height);
+//     ctx.clearRect(0, 0, e.width, e.height);
+//     ctx.drawImage(image, 0, 0, e.width, e.height);
+
+//     var selectedColor = my_color;
+
+   
+
+//     ctx.globalCompositeOperation = 'multiply';
+//     ctx.fillStyle = `rgba(${parseInt(selectedColor.slice(1, 3), 16)}, ${parseInt(selectedColor.slice(3, 5), 16)}, ${parseInt(selectedColor.slice(5, 7), 16)}, ${selectedOpacity})`;
+//     ctx.fillRect(0, 0, e.width, e.height);
+//     ctx.globalCompositeOperation = 'source-over';
+// });
+// }
+
+
+function applyColorOverlay_multiple(my_color) {
+
+ document.querySelectorAll('.myCanvas').forEach(e=>{
+  var ctx = e.getContext('2d');
+  var image = document.querySelector('.image_grid_parent.append_grid ul li.i_am_selected img');
+
+
+  e.width = image.width;
+  e.height = image.height;
+
+  ctx.clearRect(0, 0, e.width, e.height);
+  ctx.drawImage(image, 0, 0, e.width, e.height);
 
   var selectedColor = my_color;
 
-  // Clear the main canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Apply color overlay only to non-transparent pixels using the mask
-  ctx.globalCompositeOperation = 'source-over';
-  ctx.drawImage(maskCanvas, 0, 0);
-  ctx.globalCompositeOperation = 'source-in';
+  ctx.globalCompositeOperation = 'multiply';
   ctx.fillStyle = `rgba(${parseInt(selectedColor.slice(1, 3), 16)}, ${parseInt(selectedColor.slice(3, 5), 16)}, ${parseInt(selectedColor.slice(5, 7), 16)}, ${selectedOpacity})`;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0, 0, e.width, e.height);
+  ctx.globalCompositeOperation = 'source-over';
+
 });
-}
+
+  }
